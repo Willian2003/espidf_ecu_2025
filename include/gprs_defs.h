57528581fae01433f25e04e3d5438dea2e879769
joaogabrieldonato <jgsd.2001@gmail.com>
2023-02-06 21:30:29 -0300

    fix gprs pin

--- include/gprs_defs.h
+++ include/gprs_defs.h
@@ -3,6 +3,8 @@
 
 
 #define SerialAT Serial2 // Set serial for AT commands (to the module)
+#define MODEM_TX 33
+#define MODEM_RX 32
 #define PORT 1883
 // Configure TinyGSM library
 #define TINY_GSM_MODEM_SIM800   // Modem is SIM800
