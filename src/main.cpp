6facf98be68ad53185c5b8048317f8f07f06b6b7
joaogabrieldonato <jgsd.2001@gmail.com>
2023-02-10 20:03:19 -0300

    Fix mqtt Reconecting

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ const char *server = "64.227.19.172";
 char msg[MSG_BUFFER_SIZE];
 char payload_char[MSG_BUFFER_SIZE];
 
-bool mqttflag;
+bool mqtt_flag;
 
 // Define timeout time in milliseconds,0 (example: 2000ms = 2s)
 const long timeoutTime = 1000;
@@ -449,16 +449,16 @@ void gsmReconnect()
     Serial.println("Reconecting to MQTT Broker..");
     String clientId = "ESP32Client-";
     clientId += String(random(0xffff), HEX);
-    mqttflag=false;
+    mqtt_flag=false;
 
-    if (!mqttflag) {
+    if (!mqtt_flag) {
         if (mqttClient.connect(clientId.c_str(), "manguebaja", "aratucampeao", "/esp-connected", 2, true, "Offline", true))
       {
         sprintf(msg, "%s", "Online");
         mqttClient.publish("/esp-connected", msg);
         memset(msg, 0, sizeof(msg));
         Serial.println("Connected.");
-        mqttflag=true;
+        mqtt_flag=true;
 
         /* Subscribe to topics */
         mqttClient.subscribe("/esp-test");
