035e3d7cc1fd99fbd0e6cd92b34d3711bfc98ae0
joaogabrieldonato <jgsd.2001@gmail.com>
2023-10-17 23:01:15 -0300

    Add angle struct

--- include/packets.h
+++ include/packets.h
@@ -17,6 +17,12 @@ typedef struct
     int16_t dps_z;
 } imu_dps_t;
 
+typedef struct 
+{
+    int16_t Roll;
+    int16_t Pitch;
+} Angle_t;
+
 typedef struct
 {
     /* BMU DATAS */
@@ -32,6 +38,7 @@ typedef struct
     /* FRONT DATAS */
     imu_dps_t imu_dps;
     imu_acc_t imu_acc;
+    Angle_t Angle;
     uint16_t speed;
     /* FLAGS */
     uint8_t SOT;   // NC | NC | NC | DEVAGAR | PISAR | COMB_STOP | BOX_MSG | ON=1/OF=0
