f4c596ca33dfa8adce9536708403a2ff773173b2
joaogabrieldonato <jgsd.2001@gmail.com>
2023-12-09 15:20:33 -0300

    Remove Saving Period

--- include/saving.h
+++ include/saving.h
@@ -6,10 +6,7 @@
 #include "hardware_defs.h"
 #include "packets.h"
 
-#define SAVING_PERIOD 5
-
 // if testing is needed, raising the value above may help the analisys
-
 char file_name[20];
 File root;
 File dataFile;
