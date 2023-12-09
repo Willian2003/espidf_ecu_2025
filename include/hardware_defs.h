9e8b04aa02efc47bbbe65e4eb0899b2f9a315cae
joaogabrieldonato <jgsd.2001@gmail.com>
2023-12-09 12:31:17 -0300

    include gpio.h

--- include/hardware_defs.h
+++ include/hardware_defs.h
@@ -1,29 +1,33 @@
 #ifndef HARDWARE_DEFS_H_
 #define HARDWARE_DEFS_H_
 
+/*==============================*/
+    #include "driver/gpio.h"
+/*==============================*/
+
 /* pinout definitions */
 // LEDS
-#define EMBEDDED_LED LED_BUILTIN // Turned on on failiure
-#define DEBUG_LED    25
+#define EMBEDDED_LED  LED_BUILTIN // Turned on on failiure
+#define DEBUG_LED     GPIO_NUM_25
 
 /* SD */ 
-#define SD_CS  5 
+#define SD_CS         GPIO_NUM_5 
 // SPI BUS
 // VSPI
-#define MISO   19
-#define MOSI   23
-#define SCK    18
+#define MISO          GPIO_NUM_19
+#define MOSI          GPIO_NUM_23
+#define SCK           GPIO_NUM_18
 
 /* CAN PINS */ 
-#define CAN_TX 21  
-#define CAN_RX 22
+#define CAN_RX_id     GPIO_NUM_21  
+#define CAN_TX_id     GPIO_NUM_22
 // MCP2515
-#define CAN_CS        4
-#define CAN_INTERRUPT 22
+//#define CAN_CS        GPIO_NUM_4
+//#define CAN_INTERRUPT GPIO_NUM_22
 
 /* GPRS */
-#define MODEM_RST 
-#define GPRS_TX   17
-#define GPRS_RX   16
+#define MODEM_RST
+#define MODEM_TX      GPIO_NUM_17
+#define MODEM_RX      GPIO_NUM_16 
 
 #endif
